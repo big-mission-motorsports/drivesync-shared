d7c2a60772ac1a5ee04ccb975ba700bb32c06e97
Brian Griggs <bgriggs@econolite.com>
2020-10-25 16:14:59 -0600

    App updates for config chagnes. Fixes #170

--- aspnet-core/common/BigMission.DeviceApp.Shared/CanAppConfigDto.cs
+++ aspnet-core/common/BigMission.DeviceApp.Shared/CanAppConfigDto.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace BigMission.DeviceApp.Shared
 {
@@ -12,14 +10,10 @@ namespace BigMission.DeviceApp.Shared
         public int Id { get; set; }
         public int DeviceAppId { get; set; }
         public Guid ConfigurationId { get; set; }
-        public string KafkaEndpoint { get; set; }
         public string KafkaConnectionString { get; set; }
         public string KafkaDataTopic { get; set; }
         public string KafkaHeartbeatTopic { get; set; }
         public string KafkaCommandTopic { get; set; }
-        public string BlobStorageConnStr { get; set; }
-        public string BlobContainer { get; set; }
-        public string CanDumpPath { get; set; }
         public string CanCmd { get; set; }
         public string CanArg { get; set; }
         public string CanBitrate { get; set; }

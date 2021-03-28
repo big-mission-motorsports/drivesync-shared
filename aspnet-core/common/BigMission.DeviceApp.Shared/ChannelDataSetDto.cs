d19fe07b07169d0cde9c16c8780228e099af4481
Brian Griggs <bgriggs@econolite.com>
2021-03-28 13:48:00 -0600

    Reducing device app status size. Fixes #239

--- aspnet-core/common/BigMission.DeviceApp.Shared/ChannelDataSetDto.cs
+++ aspnet-core/common/BigMission.DeviceApp.Shared/ChannelDataSetDto.cs
@@ -1,20 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Newtonsoft.Json;
+using System;
 
 namespace BigMission.DeviceApp.Shared
 {
     public class ChannelDataSetDto
     {
+        [JsonProperty("id")]
         public int DeviceAppId { get; set; }
+        [JsonProperty("t")]
         public DateTime Timestamp { get; set; }
 
         /// <summary>
         /// Indicates whether these channels are virtual updates.  Otherwise,
         /// they originated in the car.
         /// </summary>
+        [JsonProperty("v")] 
         public bool IsVirtual { get; set; }
 
+        [JsonProperty("d")]
         public ChannelStatusDto[] Data { get; set; }
     }
 }
--- aspnet-core/common/BigMission.DeviceApp.Shared/ChannelStatusDto.cs
+++ aspnet-core/common/BigMission.DeviceApp.Shared/ChannelStatusDto.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 
 namespace BigMission.DeviceApp.Shared
 {
     public class ChannelStatusDto
     {
+        [JsonProperty("t")]
         public DateTime Timestamp { get; set; }
+        [JsonProperty("did")]
         public int DeviceAppId { get; set; }
+        [JsonProperty("cid")]
         public int ChannelId { get; set; }
+        [JsonProperty("v")]
         public float Value { get; set; }
     }
 }

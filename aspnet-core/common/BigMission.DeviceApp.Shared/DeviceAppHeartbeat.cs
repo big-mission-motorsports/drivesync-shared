a8f8d7dae3582ec78498bd797e92de19fd3b8440
Brian Griggs <bgriggs@econolite.com>
2021-03-27 19:14:12 -0600

    Changing device status to use redis. Fixes #259

---
+++ aspnet-core/common/BigMission.DeviceApp.Shared/DeviceAppHeartbeat.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace BigMission.DeviceApp.Shared
+{
+    public class DeviceAppHeartbeat
+    {
+        [JsonProperty("id")]
+        public int DeviceAppId { get; set; }
+        [JsonProperty("ts")]
+        public DateTime Timestamp { get; set; }
+        [JsonProperty("cid")]
+        public Guid ConfigurationId { get; set; }
+        [JsonProperty("cpu")]
+        public string CpuUsage { get; set; }
+        [JsonProperty("ll")]
+        public string LogLevel { get; set; }
+        [JsonIgnore]
+        public string Logs { get; set; }
+    }
+}

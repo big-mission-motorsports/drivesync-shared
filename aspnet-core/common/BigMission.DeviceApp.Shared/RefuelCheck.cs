c98c68875ae410f7a46b9794241293adc543abec
Brian Griggs <brian@bigmissionmotorsports.com>
2021-07-15 18:34:46 -0600

    Fuel range basic support. #311

--- aspnet-core/common/BigMission.DeviceApp.Shared/RefuelCheck.cs
+++ aspnet-core/common/BigMission.DeviceApp.Shared/RefuelCheck.cs
@@ -1,22 +1,40 @@
-﻿using System;
+﻿using BigMission.TestHelpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace BigMission.DeviceApp.Shared
 {
+    /// <summary>
+    /// Determines when the car is being refuled.  This is based on an 
+    /// increase in fuel level when the car is stationary.  
+    /// </summary>
     public class RefuelCheck
     {
+        private readonly IDateTimeHelper dateTimeHelper;
         private readonly List<float> fuelLevelSamples = new List<float>();
-        private const int MAX_SPEED_CHECK = 4;
+        /// <summary>
+        /// Consider this speed to be stopped since GPS can float around.
+        /// </summary>
+        private const int MAX_SPEED_CHECK_MPH = 5;
         private const int MAX_FL_SAMPLES = 10;
+        /// <summary>
+        /// This is the level increase at which we'll consider the car being refuled.
+        /// </summary>
         private const float FL_INC_THRESHOLD = 2.5f;
         private readonly TimeSpan MaxRefuelInterval = TimeSpan.FromMinutes(15);
         private DateTime lastRefuel;
 
+
+        public RefuelCheck(IDateTimeHelper dateTimeHelper)
+        {
+            this.dateTimeHelper = dateTimeHelper;
+        }
+
+
         public bool IsRefuling(int speed, float fuelLevel)
         {
-            if (speed < MAX_SPEED_CHECK)
+            if (speed < MAX_SPEED_CHECK_MPH)
             {
                 while (fuelLevelSamples.Count > MAX_FL_SAMPLES)
                 {
@@ -31,10 +49,11 @@ namespace BigMission.DeviceApp.Shared
                     var delta = fuelLevelSamples.Last() - fuelLevelSamples.First();
                     if (delta >= FL_INC_THRESHOLD)
                     {
-                        var timeInt = DateTime.Now - lastRefuel;
+                        var timeInt = dateTimeHelper.UtcNow - lastRefuel;
                         if (timeInt > MaxRefuelInterval)
                         {
-                            lastRefuel = DateTime.Now;
+                            lastRefuel = dateTimeHelper.UtcNow;
+                            fuelLevelSamples.Clear();
                             return true;
                         }
                     }

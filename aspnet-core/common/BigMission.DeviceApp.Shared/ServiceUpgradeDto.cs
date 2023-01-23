0dfe76a4d5127e693efdcad43c9ab3844a793dfa
Brian Griggs <brian@bigmissionmotorsports.com>
2023-01-22 21:36:24 -0700

    Adding code to download new version of the service. Fixes #545

---
+++ aspnet-core/common/BigMission.DeviceApp.Shared/ServiceUpgradeDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BigMission.DeviceApp.Shared
+{
+    public class ServiceUpgradeDto
+    {
+        public string Filename { get; set; }
+        public string Hash { get; set; }
+    }
+}
